85823aa01fc4fb443ff3c86230b192a29eb14332
Massimo Caputo <massimo.caputo@accenture.com>
2023-02-11 17:50:46 +0100

    add doc to dto

--- src/main/java/com/acn/nemo/dto/JobsDto.java
+++ src/main/java/com/acn/nemo/dto/JobsDto.java
@@ -6,17 +6,25 @@ import lombok.Data;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+/**
+ * Instantiates a new jobs dto.
+ */
 @Data
 public class JobsDto implements Serializable {
 	
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 7186381847828240537L;
 
+	/** The job id. */
 	private String jobId;
 
+    /** The job title. */
     private String jobTitle;
 
+    /** The min salary. */
     private BigDecimal minSalary;
 
+    /** The max salary. */
     private BigDecimal maxSalary;
 
 }
--- src/main/java/com/acn/nemo/dto/RegionsDto.java
+++ src/main/java/com/acn/nemo/dto/RegionsDto.java
@@ -6,15 +6,22 @@ import java.util.List;
 
 import lombok.Data;
 
+/**
+ * Instantiates a new regions dto.
+ */
 @Data
 public class RegionsDto implements Serializable {
     
+    /** The Constant serialVersionUID. */
     private static final long serialVersionUID = -2375087521949932771L;
 
+	/** The region id. */
 	private String regionId;
 
+    /** The region name. */
     private String regionName;
     
+    /** The countries. */
     private List<CountryDto> countries;
 
 }

79750298b6c737735c8eeb7b58f5dfa7a9bae151
Massimo Caputo <massimo.caputo@accenture.com>
2023-02-17 14:12:29 +0100

    Comment

--- src/main/java/com/acn/nemo/mapper/CountriesMapper.java
+++ src/main/java/com/acn/nemo/mapper/CountriesMapper.java
@@ -4,13 +4,28 @@ import org.mapstruct.Mapper;
 
 import com.acn.nemo.dto.CountryDto;
 import com.acn.nemo.model.Country;
-import org.mapstruct.Mapping;
 
+/**
+ * The Interface CountriesMapper.
+ */
 @Mapper(componentModel = "spring")
 public interface CountriesMapper {
 
+    /**
+     * Model to dto.
+     *
+     * @param countries the countries
+     * @return the country dto
+     */
     CountryDto modelToDto(Country countries);
 
+    /**
+     * Dto to to model.
+     *
+     * @param countriesDto the countries dto
+     * @return the country
+     */
     Country dtoToToModel(CountryDto countriesDto);
-
+	
+    
 }

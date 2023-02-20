0911b526e4c345fc528401a87130318796710d3e
Massimo Caputo <massimo.caputo@accenture.com>
2023-02-20 16:04:12 +0100

    modifica nome

--- src/main/java/com/acn/nemo/exception/ValidationErrorHandling.java
+++ src/main/java/com/acn/nemo/exception/ValidationErrorHandling.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ErrorHandlingValidation {
+public class ValidationErrorHandling {
 
 	@ExceptionHandler(ConstraintViolationException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)

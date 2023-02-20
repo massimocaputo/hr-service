ca7dedd463dc24db7d45c3eb36fdc56ab2b8b67d
Massimo Caputo <massimo.caputo@accenture.com>
2023-02-20 17:29:14 +0100

    personalize message locale

--- src/main/java/com/acn/nemo/config/CustomMessageSourceConfiguration.java
+++ src/main/java/com/acn/nemo/config/CustomMessageSourceConfiguration.java
@@ -3,6 +3,7 @@ package com.acn.nemo.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -12,28 +13,29 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @Configuration
 public class CustomMessageSourceConfiguration {
 
-	/**
-	 * Message source.
-	 *
-	 * @return the message source
-	 */
-	@Bean
-	public MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("classpath:messages");
-		messageSource.setDefaultEncoding("UTF-8");
-		return messageSource;
-	}
+    /**
+     * Message source.
+     *
+     * @return the message source
+     */
+    @Bean
+    MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setDefaultLocale(LocaleContextHolder.getLocale());
+        return messageSource;
+    }
 
-	/**
-	 * Gets the validator.
-	 *
-	 * @return the validator
-	 */
-	@Bean
-	public LocalValidatorFactoryBean getValidator() {
-		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-		bean.setValidationMessageSource(messageSource());
-		return bean;
-	}
+    /**
+     * Gets the validator.
+     *
+     * @return the validator
+     */
+    @Bean
+    LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
 }

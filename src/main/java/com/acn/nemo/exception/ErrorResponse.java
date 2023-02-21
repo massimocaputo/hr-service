a5929874425e0aca2db9f0076c45e4e110f5e415
Massimo Caputo <massimo.caputo@accenture.com>
2023-02-21 20:56:15 +0100

    new error class

---
+++ src/main/java/com/acn/nemo/exception/ErrorResponse.java
@@ -0,0 +1,40 @@
+package com.acn.nemo.exception;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+/**
+ * Gets the message error.
+ *
+ * @return the message error
+ */
+@Getter
+
+/**
+ * Sets the message error.
+ *
+ * @param messageError the new message error
+ */
+@Setter
+
+/**
+ * To string.
+ *
+ * @return the java.lang. string
+ */
+@Builder
+public class ErrorResponse {
+	
+	/** The code. */
+	private int code;
+	
+	/** The message error. */
+	private List<String> message;
+
+}

c5abd92c401ee052fc839042c8892a024e945439
Massimo Caputo <massimo.caputo@accenture.com>
2023-02-25 14:55:09 +0100

    modify openApi

--- src/main/java/com/acn/nemo/config/OpenApiConfig.java
+++ src/main/java/com/acn/nemo/config/OpenApiConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class OpenApiConfig {
@@ -15,6 +17,19 @@ public class OpenApiConfig {
 		return new OpenAPI()
 				.components(new Components())
 				.info(new Info()
+						.title("Region Web Service API")
+						.description("Spring Boot Rest Api Region")
+						.termsOfService("terms")
+						.license(new License()
+								.name("GNU General Public License")
+								.url("https://www.gnu.org/licenses/gpl-3.0.en.html")
+								)
+						.version("1.0")
+						.contact(new Contact()
+								.name("Massimo Caputo")
+								.email("massimo.caputo@accenture.com")
+								.url("http://wwww.google.com")
+								)
 						);
 		
 	}

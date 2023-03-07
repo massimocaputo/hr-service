09742aaa0cee254cf8e0de160edb56a46cbb17b0
Massimo Caputo <massimo.caputo@accenture.com>
2023-03-07 22:34:45 +0100

    openApi

--- src/main/java/com/acn/nemo/config/OpenApiConfig.java
+++ src/main/java/com/acn/nemo/config/OpenApiConfig.java
@@ -11,27 +11,27 @@ import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class OpenApiConfig {
-	
-	@Bean
-	public OpenAPI customOpenAp() {
-		return new OpenAPI()
-				.components(new Components())
-				.info(new Info()
-						.title("Region Web Service API")
-						.description("Spring Boot Rest Api Region")
-						.termsOfService("terms")
-						.license(new License()
-								.name("GNU General Public License")
-								.url("https://www.gnu.org/licenses/gpl-3.0.en.html")
-								)
-						.version("1.0")
-						.contact(new Contact()
-								.name("Massimo Caputo")
-								.email("massimo.caputo@accenture.com")
-								.url("http://wwww.google.com")
-								)
-						);
-		
-	}
+
+    @Bean
+    OpenAPI customOpenAp() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Region Web Service API")
+                        .description("Spring Boot Rest Api Region")
+                        .termsOfService("terms")
+                        .license(new License()
+                                .name("GNU General Public License")
+                                .url("https://www.gnu.org/licenses/gpl-3.0.en.html")
+                        )
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Massimo Caputo")
+                                .email("massimo.caputo@accenture.com")
+                                .url("http://wwww.google.com")
+                        )
+                );
+
+    }
 
 }

090c0f173ea7d9268189b4adc4e0cabdfd3256bd
massimo.caputo@icloud.com <massimo.caputo@accenture.com>
2023-08-09 18:22:19 +0200

    Fill Location Dto

--- src/main/java/com/acn/nemo/dtos/LocationDto.java
+++ src/main/java/com/acn/nemo/dtos/LocationDto.java
@@ -4,5 +4,12 @@ import lombok.Data;
 
 @Data
 public class LocationDto {
+	
+	private Long locationId;
+	private String streetAddress;
+	private String postalCode;
+	private String city;
+	private String stateProvince;
+	private String countryId;
 
 }

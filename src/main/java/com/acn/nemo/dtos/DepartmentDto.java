cc3645db215db46f2b942d01e5f6a3f0a8a1cbd7
Massimo, Caputo <massimo.caputo@accenture.com>
2023-08-10 19:58:26 +0200

    creazione dept dto

---
+++ src/main/java/com/acn/nemo/dtos/DepartmentDto.java
@@ -0,0 +1,49 @@
+package com.acn.nemo.dtos;
+
+import com.acn.nemo.model.Location;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Gets the department name.
+ *
+ * @return the department name
+ */
+@Getter
+
+/**
+ * Sets the department name.
+ *
+ * @param departmentName the new department name
+ */
+@Setter
+
+/**
+ * Instantiates a new department dto.
+ */
+@NoArgsConstructor
+
+/**
+ * Instantiates a new department dto.
+ *
+ * @param departmentName the department name
+ */
+@AllArgsConstructor
+
+/**
+ * To string.
+ *
+ * @return the java.lang. string
+ */
+@Builder
+public class DepartmentDto {
+
+	
+	 /** The department name. */
+ 	private String departmentName;
+ 	
+}

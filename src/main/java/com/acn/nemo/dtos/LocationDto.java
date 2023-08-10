e38b58a8738d6259e6b3cf1ced747b87cee0df01
Massimo, Caputo <massimo.caputo@accenture.com>
2023-08-10 19:58:54 +0200

    modifica dto con rimozione @data

--- src/main/java/com/acn/nemo/dtos/LocationDto.java
+++ src/main/java/com/acn/nemo/dtos/LocationDto.java
@@ -1,15 +1,68 @@
 package com.acn.nemo.dtos;
 
-import lombok.Data;
+import java.util.List;
 
-@Data
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Gets the department dtos.
+ *
+ * @return the department dtos
+ */
+@Getter
+
+/**
+ * Sets the department dtos.
+ *
+ * @param departmentDtos the new department dtos
+ */
+@Setter
+
+/**
+ * Instantiates a new location dto.
+ */
+@NoArgsConstructor
+
+/**
+ * Instantiates a new location dto.
+ *
+ * @param locationId the location id
+ * @param streetAddress the street address
+ * @param postalCode the postal code
+ * @param city the city
+ * @param stateProvince the state province
+ * @param departmentDtos the department dtos
+ */
+@AllArgsConstructor
+
+/**
+ * To string.
+ *
+ * @return the java.lang. string
+ */
+@Builder
 public class LocationDto {
 	
+	/** The location id. */
 	private Long locationId;
+	
+	/** The street address. */
 	private String streetAddress;
+	
+	/** The postal code. */
 	private String postalCode;
+	
+	/** The city. */
 	private String city;
+	
+	/** The state province. */
 	private String stateProvince;
-	private String countryId;
+	
+	/** The department dtos. */
+	private List<DepartmentDto> departmentDtos;
 
 }
--- src/main/java/com/acn/nemo/dtos/RegionDto.java
+++ src/main/java/com/acn/nemo/dtos/RegionDto.java
@@ -1,10 +1,55 @@
 package com.acn.nemo.dtos;
 
-import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
 
-@Data
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Gets the countries.
+ *
+ * @return the countries
+ */
+@Getter
+
+/**
+ * Sets the countries.
+ *
+ * @param countries the new countries
+ */
+@Setter
+
+/**
+ * Instantiates a new region dto.
+ */
+@NoArgsConstructor
+
+/**
+ * Instantiates a new region dto.
+ *
+ * @param regionName the region name
+ * @param countries the countries
+ */
+@AllArgsConstructor
+
+/**
+ * To string.
+ *
+ * @return the java.lang. string
+ */
+@Builder
 public class RegionDto {
 	
+	
+	private String id;
+	
+	/** The region name. */
 	private String regionName;
 
+	/** The countries. */
+	private List<CountryDto> countries; 
 }
